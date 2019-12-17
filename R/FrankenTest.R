ed9f4ad9f82378880e959f7b33c5075c11d339e3
ldvroditi <56916052+ldvroditi@users.noreply.github.com>
2019-12-17 16:29:16 +0100

    Add files via upload

---
+++ R/FrankenTest.R
@@ -0,0 +1,10 @@
+#' Franken Package Test
+#'
+#' Contains functions required for Franken clustering procedure
+#'
+#' @doctype Package
+#'
+#' @author Laura De Vargas Roditi \email{laura.devargasroditi@uzh.ch}
+#'
+#' @name FrankenTest
+NULL
